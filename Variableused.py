9a0677aa4dc3b3a38e1152711835f490091b0a94
Barsha <mebarsha1@gmail.com>
2019-11-11 10:11:26 +0545

    Training

---
+++ Variableused.py
@@ -0,0 +1,13 @@
+
+name=input('Enter username')
+age=input('Enter age')
+first =int(input('Enter the value of first'))
+second =int(input('Enter the value of second'))
+Result=first+second
+print(Result)
+Result=first-second
+print(Result)
+print(type(Result))
+Result=(first/second)
+print(Result)
+print(type(Result))
